abe0278af9750d03e295d605df8751a61f0f9f6d
kimjmin <kimjmin@gmail.com>
2022-12-30 11:54:51 +0900

    event.preventDefault() 실습

--- nomadcoders/vanillajs/app.js
+++ nomadcoders/vanillajs/app.js
@@ -1,1 +1,16 @@
-// 실제 앱 개발 시작
+// 4.0 실제 앱 개발 시작
+const loginForm = document.querySelector('#login-form');
+const logintInput = loginForm.querySelector("input");
+const logintbutton = loginForm.querySelector("button");
+
+
+function onLoginBtnClick(event){
+  event.preventDefault();  // stop default behavior
+  console.log(event);
+  const userName = logintInput.value;
+  console.log(userName);
+}
+
+// logintbutton.addEventListener("click", onLoginBtnClick)
+loginForm.addEventListener("submit", onLoginBtnClick);
+// onLoginBtnClick(info) 정보를 담고 반환함
